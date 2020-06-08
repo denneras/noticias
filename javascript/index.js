f7ef95e54d6515e1e6c6d4f756fd0f52b6c6ee4b
Denner Almeida <denner.almeidasilva@gmail.com>
2020-06-07 22:40:42 -0300

    finalizado css

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,12 @@
 const body = document.querySelector('body');
 const btOpen = document.getElementById('btn-open');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
+const btnFooter = document.querySelector('#footer button');
 
 
-
+btnFooter.addEventListener('click', e => {
+    window.scrollTo(0, 0);
+})
 
 
 
